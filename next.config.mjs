249979e23234e6078dec6afc246eca18e8a5b429
Mouniesh <iammounieshv@gmail.com>
2025-01-19 17:26:46 +0530

    modified conf

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'lh3.googleusercontent.com',
-        pathname: '/**',
-      },
-    ],
-  },
-  webpack(config) {
-    config.experiments.topLevelAwait = true;
-    return config;
-  },
-};
-
-export default nextConfig;
+    experimental: {
+      appDir: true,
+      serverComponentsExternalPackages: ["mongoose"],
+    },
+    images: {
+      domains: ['lh3.googleusercontent.com'],
+    },
+    webpack(config) {
+      config.experiments = {
+        ...config.experiments,
+        topLevelAwait: true,
+      };
+      return config;
+    },
+  };
+  
+  export default nextConfig;
+  
