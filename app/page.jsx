0f3b16b75218d22d6abc3265dbd2d205b564868b
Mouniesh <iammounieshv@gmail.com>
2025-01-19 17:15:20 +0530

    bug fixes

--- app/page.jsx
+++ app/page.jsx
@@ -7,7 +7,6 @@ const Home = () => {
    <section className="w-full flex-center flex-col">
     <h1 className="head_text text-center">
         Discover & Share 
-        <br />
     <br className="max-md:hidden" />
     <span className="orange_gradient text-center">
         AI-Powered Prompts
--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,4 +1,4 @@
-'use client'
+"use client"
 
 import PromptCard from "./PromptCard"
 import { useEffect, useState } from "react"
