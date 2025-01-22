fc3a93c0c41ed40d7261acc178b38578c6b38b3b
Mouniesh <iammounieshv@gmail.com>
2025-01-22 22:35:37 +0530

    create-prompt : loading func

---
+++ app/create-prompt/loading.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const loading = () => {
+  return (
+    <>
+    <div className='w-full flex-center'>
+      <div className='flex flex-col gap-4 items-center'>
+       
+        <p className='text-lg text-gray-600'>Loading...</p>
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default loading
